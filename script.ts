907de1ab7b93812e150b3228d87bb1a8cb489c32
chiappettafran <franciscomartinezchiappetta@gmail.com>
2024-11-15 16:15:11 -0300

    Initial commit

---
+++ script.ts
@@ -0,0 +1,18 @@
+let interMiami: number = 11
+let fcDallas: number = 10
+let messi: number = 1
+let juegaMessi: boolean = false
+
+function jugar(equipo1: number, equipo2: number, juegaMessi: boolean): void {
+    let motivo: string = ''
+    if (juegaMessi) {
+        equipo1 += messi
+        motivo = "porque juega Messi"
+    }
+
+    if (equipo1 > equipo2) console.log(`Gana Inter Miami ${motivo}`)
+    if (equipo1 == equipo2) console.log('Empatan')
+    if (equipo1 < equipo2) console.log('Gana Dallas')
+}
+
+jugar(interMiami, fcDallas, juegaMessi)
